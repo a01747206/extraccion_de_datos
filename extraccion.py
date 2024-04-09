2463144bacb69812cc4d8139abe58076a8a1820f
a01747206 <165735282+a01747206@users.noreply.github.com>
2024-04-08 19:52:53 -0600

    entregables del 4 al 9

--- extraccion.py
+++ extraccion.py
@@ -19,3 +19,37 @@ filtro2.to_csv("Entregable2.csv")
 filtro3=df.iloc[ : , [1, 3, 4,10]]  
 print(filtro3)
 filtro3.to_csv("Entregable3.csv")
+
+
+df1 = pd.read_excel("detalle_precios_productos_fabricados_2022.xlsx", index_col=1)
+
+
+#Filtro de subtotal
+filtro4=df1.loc[["2022-11-22","2022-12-23"], ["SUBTOTAL_PARTIDA"]]
+print(filtro4)
+filtro4.to_csv("Entregable4.csv")
+
+#Filtro por cabecera
+filtro5=df.head(8)
+print(filtro5)
+filtro5.to_csv("Entregable5.csv")
+
+#Filtro por comparación
+filtro6=df[df["SUBTOTAL_PARTIDA"] > 77000]
+print(filtro6)
+filtro6.to_csv("Entregable6.csv")
+
+#Filtro de subtotal y fecha
+filtro7=df[(df["SUBTOTAL_PARTIDA"] > 77000) & (df["FECHA_DOC"] == "2022-05-24")]
+print(filtro7)
+filtro7.to_csv("Entregable7.csv")
+
+#Filtro de subtotal mayor a 77000 o fecha de 2022-05-24
+filtro8=df[(df["SUBTOTAL_PARTIDA"] > 77000)| (df["FECHA_DOC"] == "2022-05-24")]
+print(filtro8)
+filtro8.to_csv("Entregable8.csv")
+
+##Filtro de subtotal mayor a 77000 y fecha diferente a 2022-05-24
+filtro9=df[~(df["SUBTOTAL_PARTIDA"] > 77000) & ~(df["FECHA_DOC"] == "2022-05-24")]
+print(filtro9)
+filtro9.to_csv("Entregable9.csv")
